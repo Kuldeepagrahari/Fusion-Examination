3cfcddd691ef536f67df10af807b2c435ba0ff43
Kuldeep Agrahari <133698085+Kuldeepagrahari@users.noreply.github.com>
2024-10-08 00:12:40 +0530

    routing in the pages

--- UI/src/App.jsx
+++ UI/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import {Route, Routes} from "react-router-dom"
+import SubmitGardes from './pages/SubmitGrades/SubmitGardes'
+import VerifyGrades from './pages/VerifyGrades/VerifyGrades'
+import Announcement from './pages/Announcement/Announcement'
+import GenerateTranscript from './pages/GenerateTranscript/GenerateTranscript'
+import SeatingPlan from "./pages/SeatingPlan/SeatingPlan"
+import Navbar from './components/Navbar/Navbar'
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <Routes>
+        <Route path='/submit-grades' element={<SubmitGardes/>}></Route>
+        <Route path='/verify-grades' element={<VerifyGrades/>}></Route>
+        <Route path='/announcement' element={<Announcement/>}></Route>
+        <Route path='/generate-transcript' element={<GenerateTranscript/>}></Route>
+        <Route path='/seating-plan' element={<SeatingPlan />}></Route>
+      </Routes>
     </div>
   )
 }
--- UI/src/pages/GenerateTranscript/GenerateTranscript.jsx
+++ UI/src/pages/GenerateTranscript/GenerateTranscript.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GenerateTranscript = () => {
   return (
     <div>
-      
+      <h1>generate</h1>
     </div>
   )
 }
